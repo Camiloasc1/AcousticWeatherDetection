f19fde59c646807ff23cdc33100580ef8db15389
caasanchezcu <caasanchezcu@unal.edu.co>
2016-05-01 16:30:04 -0500

    Analyse audio recorded from mic.

--- src/main.py
+++ src/main.py
@@ -1,41 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io import wavfile
 from signals.fourier import fourier
 from signals.analysis import ClimbingAgent
+from audio.audio import play, record, read, write
 
 
-def plot(signal, delta, xf, yf, agents):
+def main():
+    rate = 44100
+    length = 5
+    signal = record(rate, length)
+    xf, yf = fourier(signal, rate)
+    agents = find_peaks(length, yf)
+    plot(signal, rate, length, xf, yf, agents)
+
+
+def plot(signal, rate, delta, xf, yf, agents):
     plt.subplot(2, 1, 1)
-    plt.plot(np.arange(len(signal)), signal)
+    plt.plot(np.arange(len(signal)) / rate, signal)
     plt.subplot(2, 1, 2)
     plt.plot(xf, yf)
     plt.plot([a.position / delta for a in agents], [a.value for a in agents], 'r*')
     plt.show()
 
 
-def read(start, end):
-    sample_rate, signal = wavfile.read('Mon28Mar16.wav')
-    signal = signal[start * sample_rate:end * sample_rate, 0] / (2. ** 15)  # Left stereo
-    return sample_rate, signal
-
-
-def main():
-    start = 0 * 60
-    end = start + 15
-    delta = end - start  # Also is equal to the number of samples between integral frequency values
-    sample_rate, signal = read(start, end)
-    xf, yf = fourier(signal, sample_rate)
-    xf = xf[:delta * 2500]
-    yf = yf[:delta * 2500]
-    agent_delta = delta * 10  # 10Hz
+def find_peaks(length, yf):
+    agent_delta = length * 10  # 10Hz
     agents = [ClimbingAgent(yf, i, agent_delta, 0.005, True) for i in range(agent_delta, len(yf), agent_delta * 2 + 1)]
     agents = list(filter(lambda a: a.climb().is_on_peak, agents))
-    print(len(agents))
-    print([a.position / delta for a in agents])
-    print([a.value for a in agents])
-
-    plot(signal, delta, xf, yf, agents)
+    # print(len(agents))
+    # print([a.position / length for a in agents])
+    # print([a.value for a in agents])
+    return agents
 
 
 if __name__ == "__main__":
